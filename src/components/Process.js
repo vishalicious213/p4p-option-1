9a0034dce49937f9230ecdbd683c13ae561dece8
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 23:17:17 -0700

    styling details & summary Process.js

--- src/components/Process.js
+++ src/components/Process.js
@@ -1,6 +1,10 @@
+import '../styles/App.css'
+import '../styles/Process.css'
+
 function Process() {
     return (
         <section>
+            <h3 id='process-h3'>APPLYING FOR A ROLE</h3>
             <details>
                 <summary>1. Create an Account</summary>
                 <p>The first step is Creating An Account in Place for Purpose. This involves completing a brief questionnaire to help us understand who you are, what your interests and goals are, and how your values align with Baltimore Corps. Once you submit your questionnaire, your application will be reviewed based on Values Alignment with Baltimore Corps (link to values). When your application has been approved, you will receive an email to schedule a Consultation with our team.</p>
