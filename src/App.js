77094005eedaaf5b30170ef8540ad47074398f6f
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 17:13:47 -0700

    added hero image, hero text and styled fonts & container

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Place For Purpose
+      <main>
+        <div id='main-text'>
+          <h2 id='hero-h1'>PLACE <span id='hero-span'>FOR</span> PURPOSE</h2>
+          <p className='hero-text'>is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.</p>
+        </div>
+      </main>
     </div>
   );
 }
