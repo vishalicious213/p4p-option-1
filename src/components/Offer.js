ed4a9600b56c95a4c2f9c803a8b811b8cdb0b312
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 18:25:26 -0700

    added Offering text

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -16,7 +16,10 @@ function Offer() {
         <section id='offer'>
             <h3>WHAT WE OFFER...</h3>
             <div id='offerings'>
-                <Offering img={signInImg} title='test' text='body' />
+                <Offering img={signInImg} title='Diverse Pool of Curated Talent' text='We recruit, screen, and curate skill-matched, mission-driven candidates for you!' />
+                <Offering img={signInImg} title='A Hiring Partner' text='We commit to working closely with our partners - we’re on your team!' />
+                <Offering img={signInImg} title='Equity Collaborator' text='Equity and inclusion are at the heart of what we do!' />
+                <Offering img={signInImg} title='Value of Time' text='Our team runs effective and equitable searches every 90 days!' />
             </div>
         </section>
     )
