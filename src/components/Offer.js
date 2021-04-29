9caf6e77a399c5b90ad8914d8ecd032b2729c09f
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 18:28:34 -0700

    added Offering images

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,5 +1,8 @@
 import '../styles/Offer.css'
-import signInImg from '../img/logo.png'
+import offering1 from '../img/offering-1.jpg'
+import offering2 from '../img/offering-2.jpg'
+import offering3 from '../img/offering-3.jpg'
+import offering4 from '../img/offering-4.jpg'
 
 const Offering = (props) => {
     return (
@@ -16,10 +19,10 @@ function Offer() {
         <section id='offer'>
             <h3>WHAT WE OFFER...</h3>
             <div id='offerings'>
-                <Offering img={signInImg} title='Diverse Pool of Curated Talent' text='We recruit, screen, and curate skill-matched, mission-driven candidates for you!' />
-                <Offering img={signInImg} title='A Hiring Partner' text='We commit to working closely with our partners - we’re on your team!' />
-                <Offering img={signInImg} title='Equity Collaborator' text='Equity and inclusion are at the heart of what we do!' />
-                <Offering img={signInImg} title='Value of Time' text='Our team runs effective and equitable searches every 90 days!' />
+                <Offering img={offering1} title='Diverse Pool of Curated Talent' text='We recruit, screen, and curate skill-matched, mission-driven candidates for you!' />
+                <Offering img={offering2} title='A Hiring Partner' text='We commit to working closely with our partners - we’re on your team!' />
+                <Offering img={offering3} title='Equity Collaborator' text='Equity and inclusion are at the heart of what we do!' />
+                <Offering img={offering4} title='Value of Time' text='Our team runs effective and equitable searches every 90 days!' />
             </div>
         </section>
     )
