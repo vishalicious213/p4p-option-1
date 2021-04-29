278c59422934351f0a64fb00b309bc060c538a22
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 23:04:31 -0700

    adding text to Process.js

--- src/components/Process.js
+++ src/components/Process.js
@@ -5,6 +5,11 @@ function Process() {
                 <summary>1. Create an Account</summary>
                 <p>The first step is Creating An Account in Place for Purpose. This involves completing a brief questionnaire to help us understand who you are, what your interests and goals are, and how your values align with Baltimore Corps. Once you submit your questionnaire, your application will be reviewed based on Values Alignment with Baltimore Corps (link to values). When your application has been approved, you will receive an email to schedule a Consultation with our team.</p>
             </details>
+
+            <details>
+                <summary>2. Build a Profile</summary>
+                <p>Your Profile captures your skills, capabilities and experiences (see Creating a Profile and Resume above) and we use it along with your Interest Statements (see below) to assess your qualifications for specific roles. Think of it as a more holistic resume to help us get a complete picture of your background and experience. We encourage you to use this as a master document to capture all of your amazing experiences so make sure to tag your capabilities, to showcase your contributions and impact in past and current roles. You can add to this Profile at any time and customize your Profile to share for specific positions.</p>
+            </details>
         </section>
     )
 }
