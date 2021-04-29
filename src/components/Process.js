260be5f73492a53c647a4d02ff5f08ce854be59d
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 23:05:39 -0700

    adding text to Process.js

--- src/components/Process.js
+++ src/components/Process.js
@@ -10,6 +10,26 @@ function Process() {
                 <summary>2. Build a Profile</summary>
                 <p>Your Profile captures your skills, capabilities and experiences (see Creating a Profile and Resume above) and we use it along with your Interest Statements (see below) to assess your qualifications for specific roles. Think of it as a more holistic resume to help us get a complete picture of your background and experience. We encourage you to use this as a master document to capture all of your amazing experiences so make sure to tag your capabilities, to showcase your contributions and impact in past and current roles. You can add to this Profile at any time and customize your Profile to share for specific positions.</p>
             </details>
+
+            <details>
+                <summary>3. Schedule a Consultation</summary>
+                <p>The Consultation is our opportunity to learn more about your interests and goals, to orient you to applying for roles in Place for Purpose, to learn how we can support you with your Profile, and to answer any questions about the system or process. Your profile does not need to be finished to schedule a Consultation. However, we recommend having it started and as complete as possible so we can help with specific questions. What is the difference between applying for a role and expressing interest?</p>
+            </details>
+
+            <details>
+                <summary>3. Schedule a Consultation</summary>
+                <p></p>
+            </details>
+
+            <details>
+                <summary>3. Schedule a Consultation</summary>
+                <p></p>
+            </details>
+
+            <details>
+                <summary>3. Schedule a Consultation</summary>
+                <p></p>
+            </details>
         </section>
     )
 }
