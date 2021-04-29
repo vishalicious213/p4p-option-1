1b4c0ecc8c163c190f60b79b0c10ebfffda83c7b
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 17:42:16 -0700

    drafted sign-in. added text, image & button and styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import './App.css'
+import signInImg from './img/logo.png'
 
 function App() {
   return (
@@ -7,6 +8,12 @@ function App() {
         <div id='main-text'>
           <h2 id='hero-h1'>PLACE <span id='hero-span'>FOR</span> PURPOSE</h2>
           <p className='hero-text'>is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.</p>
+        </div>
+
+        <div id='sign-in'>
+          <img id='sign-in-img' src={signInImg} alt='logo' />
+          <p>Create an account or log in to create a Baltimore Corps community profile. Get access to job opportunities and development opportunities in the Baltimore Corps network</p>
+          <button>LOG IN / SIGN UP</button>
         </div>
       </main>
     </div>
