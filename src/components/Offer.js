c113402aa96095cc31d83d48d1e6aa52a6d7647f
vishalicious213 <vishalicious213@gmail.com>
2021-04-28 18:23:03 -0700

    added Offering component to Offer.js

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,9 +1,23 @@
 import '../styles/Offer.css'
+import signInImg from '../img/logo.png'
+
+const Offering = (props) => {
+    return (
+        <div>
+            <img src={props.img} alt={props.alt} />
+            <h4>{props.title}</h4>
+            <p>{props.text}</p>
+        </div>
+    )
+}
 
 function Offer() {
     return (
         <section id='offer'>
-            OFFER
+            <h3>WHAT WE OFFER...</h3>
+            <div id='offerings'>
+                <Offering img={signInImg} title='test' text='body' />
+            </div>
         </section>
     )
 }
