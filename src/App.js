3c0bc7c8e3fa3402a43cc0772417cf0433c38930
vishalicious213 <vishalicious213@gmail.com>
2021-04-29 20:49:04 -0700

    redesigned App hero section as per input

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
       
       <main>
         <div id='main-text'>
-          <h2 id='hero-h1'>PLACE <span id='hero-span'>FOR</span> PURPOSE</h2>
-          <p className='hero-text'>is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.</p>
           <div id='sign-in'>
+            <h2 id='hero-h1'>PLACE <span id='hero-span'>FOR</span> PURPOSE</h2>
+            <p className='hero-text'>is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.</p>
             <img id='sign-in-img' src={signInImg} alt='logo' />
             <p>Create an account or log in to create a Baltimore Corps community profile. Get access to job opportunities and development opportunities in the Baltimore Corps network.</p>
             <button>LOG IN / SIGN UP</button>
@@ -24,12 +24,12 @@ function App() {
         </div>
       </main>
 
+      <Process />
       <Carousel />
       <Offer />
       <div id='p4p-container'>
         <img id='p4p-animated' src={p4p} alt='' />
       </div>
-      <Process />
       <Footer />
     </div>
   );
