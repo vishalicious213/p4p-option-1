af45bafb0fd20380103c460160eff8c51038b88e
vishalicious213 <vishalicious213@gmail.com>
2021-05-02 17:31:54 -0700

    removed Baltimore Corps image from hero section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Offer from './components/Offer'
 import Carousel from './components/Carousel'
 import Process from './components/Process'
 import Footer from './components/Footer'
-import signInImg from './img/logo.png'
+// import signInImg from './img/logo.png'
 import p4p from './img/P4P-animated.gif'
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <div id='sign-in'>
             <h2 id='hero-h1'>PLACE <span id='hero-span'>FOR</span> PURPOSE</h2>
             <p className='hero-text'>is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.</p>
-            <img id='sign-in-img' src={signInImg} alt='logo' />
+            {/* <img id='sign-in-img' src={signInImg} alt='logo' /> */}
             <p>Create an account or log in to create a Baltimore Corps community profile. Get access to job opportunities and development opportunities in the Baltimore Corps network.</p>
             <button>LOG IN / SIGN UP</button>
           </div>
